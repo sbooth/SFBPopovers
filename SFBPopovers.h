c355f6b77b9a94a61ab7cac74081186be488d731
Kyle Hankinson <kyle@hankinsoft.com>
2019-01-22 10:02:10 -0400

    + Framework headers.

---
+++ SFBPopovers.h
@@ -0,0 +1,11 @@
+@import Foundation;
+@import AppKit;
+
+//! Project version number for SFBPopovers.
+FOUNDATION_EXPORT double SFBPopoversVersionNumber;
+
+//! Project version string for SFBPopovers.
+FOUNDATION_EXPORT const unsigned char SFBPopoversVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFBPopovers/PublicHeader.h>
+#import "SFBPopover.h"
