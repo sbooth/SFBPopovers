49f0af934fa3fb6939de52d14c24a28332733cc3
Stephen F. Booth <me@sbooth.org>
2021-02-19 10:06:59 -0600

    Target macOS 10.9+ and update project layout

---
+++ SFBPopovers/SFBPopovers.h
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2011 - 2021 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/SFBPopovers
+// MIT license
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SFBPopovers.
+FOUNDATION_EXPORT double SFBPopoversVersionNumber;
+
+//! Project version string for SFBPopovers.
+FOUNDATION_EXPORT const unsigned char SFBPopoversVersionString[];
+
+#import <SFBPopovers/SFBPopover.h>
